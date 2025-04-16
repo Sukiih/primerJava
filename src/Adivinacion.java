0277d9478707d83f15f26ec3b27e17bc5e892f9f
Sukiih <vale.sgonzalez2011@hotmail.com>
2025-04-15 23:08:56 -0400

    desafio juego adivinacion

---
+++ src/Adivinacion.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Adivinacion {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int contador = 0;
+        int numeroSecreto = new Random().nextInt(100);
+
+        while(true){
+            System.out.println("Ingresa un número para adivinar el número secreto");
+            int numeroUsuario = teclado.nextInt();
+            contador++;
+
+            if (numeroSecreto == numeroUsuario){
+                System.out.println("Felicidades, encontraste el número!");
+                break;
+            } else if (numeroUsuario < numeroSecreto){
+                System.out.println("Intenta con un numero mayor");
+            } else {
+                System.out.println("El número es menor");
+            }
+        }
+    }
+}
